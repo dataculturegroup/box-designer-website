1476f71fcaefc3f3cb968599f87b7f8ca3e63a0d
Rahul B <rahulbot@gmail.com>
2018-12-21 15:39:53 -0500

    Output in points with viewport to solve InkScape bug #12

--- boxmaker/svg.py
+++ boxmaker/svg.py
@@ -9,12 +9,12 @@
 # lines. Consequently, what this driver does is collect all of the line commands and record them; on the
 # save call it concatenates them together into a set of paths.
 
-import reportlab.lib.colors as colors
 from string import Template
 
 tmpl_svg = Template("""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
-<svg width="${pixel_width}px" height="${pixel_height}px" version="1.1"
+<svg width="${point_width}pt" height="${point_height}pt" version="1.1"
+    viewBox="0 0 ${point_width} ${point_height}"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
     xml:space="preserve"
@@ -30,6 +30,8 @@ tmpl_path = Template("""        <path d="${path}" style="fill:none;stroke:${stro
 tmpl_rect = Template("""        <rect x="${x}" y="${y}" width="${w}" height="${h}"/>
 """)
 
+PIXEL_TO_POINT = 0.75
+
 
 class SVGDoc(object):
 
@@ -39,22 +41,22 @@ class SVGDoc(object):
         self.paths = []
         self.firsts = set()
         self.filename = filename
-        self.strokeColor = "black"
-        self.lineWidth = 0.5  # default is mm so we need to convert
-        self.pageSize = [0, 0]
+        self.stroke_color = "black"
+        self.line_width = 0.5  # default is mm so we need to convert
+        self.page_size = [0, 0]
         self.author = ''
 
-    def setPageSize(self, pageSize):
-        self.pageSize = pageSize
+    def setPageSize(self, page_size):
+        self.page_size = page_size
 
     def setAuthor(self, author):
         self.author = author
 
     def setStrokeColor(self, col):
-        self.strokeColor = col
+        self.stroke_color = col
 
     def setLineWidth(self, lw):
-        self.lineWidth = lw
+        self.line_width = lw
 
     def drawString(self, x, y, st):
         # String must be free of metacharacters
@@ -69,8 +71,8 @@ class SVGDoc(object):
         s += 'Z'
         self.elements.append(tmpl_path.substitute(dict(
             path=s,
-            stroke_color=self._col(self.strokeColor),
-            stroke_pixels=self._sc(self.lineWidth)
+            stroke_color=self._col(self.stroke_color),
+            stroke_pixels=self._sc(self.line_width)
             )))
 
     def drawOpenPath(self, p):
@@ -78,29 +80,35 @@ class SVGDoc(object):
         s += ''.join(["L{},{}".format(self._sc(pt[0]), self._sc(pt[1])) for pt in p[1:]])
         self.elements.append(tmpl_path.substitute(dict(
             path=s,
-            stroke_color=self._col(self.strokeColor),
-            stroke_pixels=self._sc(self.lineWidth)
+            stroke_color=self._col(self.stroke_color),
+            stroke_pixels=self._sc(self.line_width)
             )))
 
     def save(self):
         s = ''.join([e for e in self.elements])
-        pgw, pgh = self._sc(self.pageSize[0]), self._sc(self.pageSize[1])
+        pgw, pgh = self._sc(self.page_size[0]), self._sc(self.page_size[1])
+        # To support different DPI viewers, we shoudl encode the page size in points, not pixels.  This makes it work
+        #  in both InkScape and Illustrator.
         svg = tmpl_svg.substitute(dict(
-            pixel_width=pgw,
-            pixel_height=pgh,
+            point_width=self._pixel_to_point(pgw),
+            point_height=self._pixel_to_point(pgh),
             contents=s))
         ofh = open(self.filename, 'w')
         ofh.write(svg)
         ofh.close()
 
-
     # end public API
 
-    def _sc(self, v):
-        "converts from mm to pixels as a numeric string"
-        return "{:.1f}".format(v)# * 96 / 25.4)
+    @staticmethod
+    def _sc(v):
+        # converts from mm to pixels as a numeric string
+        return "{:.2f}".format(v)  # * 96 / 25.4)
 
-    def _col(self, color):
-        "generates a CSS color from a reportlab color"
+    @staticmethod
+    def _col(color):
+        # generates a CSS color from a reportlab color
         return '#'+color.hexval()[2:]
 
+    @staticmethod
+    def _pixel_to_point(pixels):
+        return "{:.2f}".format(float(pixels) * PIXEL_TO_POINT)
